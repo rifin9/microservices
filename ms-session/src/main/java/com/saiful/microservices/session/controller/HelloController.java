08cca69b69f88ad6c45d06cf44d3296101dbb53b
Saiful Arifin <saifularifin@greateasternlife.com>
2019-09-13 15:22:04 +0700

    initialize source code

---
+++ ms-session/src/main/java/com/saiful/microservices/session/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.saiful.microservices.session.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index () {
+	return "Greating from Spring Boot!";
+    }
+}
