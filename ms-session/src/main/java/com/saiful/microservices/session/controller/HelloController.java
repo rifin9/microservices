88664c04f15b9dc9d92c99cc37d2c3b7075d64c2
Saiful Arifin <say_arifin@yahoo.com>
2019-09-15 14:53:28 +0600

    add unit test class (junit + mockito)

--- ms-session/src/main/java/com/saiful/microservices/session/controller/HelloController.java
+++ ms-session/src/main/java/com/saiful/microservices/session/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.saiful.microservices.session.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
     
-    @RequestMapping("/")
+    @GetMapping("/hello")
     public String index () {
 	return "Greating from Spring Boot!";
     }
---
+++ ms-session/src/test/java/com/saiful/microservices/session/main/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.saiful.microservices.session.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+	@Autowired
+	MockMvc moMvc;
+
+	@Test
+	public void testHello() throws Exception {
+		this.moMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Greating from Spring Boot!"));
+	}
+}
